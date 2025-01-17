d002ef00b5c36395a10fe0e08a4ca93d70c9e6ca
P Nagamani <154883406+22hr1a0579@users.noreply.github.com>
2025-01-17 08:51:44 +0530

    Create timer.java

---
+++ timer.java
@@ -0,0 +1,21 @@
+'''Implement a program to read seconds values in timer and convert it into hours.
+Input Format
+seconds value
+Constraints
+no
+Output Format
+hours value'''
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        System.out.println(s/3600);
+        
+
+    }
+}
