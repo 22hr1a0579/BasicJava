884a770280a65b23b7176177cad2b34033f8249b
P Nagamani <154883406+22hr1a0579@users.noreply.github.com>
2025-01-17 09:06:23 +0530

    Create mul2.java

---
+++ mul2.java
@@ -0,0 +1,27 @@
+'''Implement a program to calcualte multiplication on given two integer values.
+
+Input Format
+
+two integer values (seperated by new line char)
+
+Constraints
+
+no
+
+Output Format
+
+multiplication'''
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a*b);
+        
+    }
+}
