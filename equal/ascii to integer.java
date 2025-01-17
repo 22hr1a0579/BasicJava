c09b30eeb3e20a984dcb0bf1d0bd7529c55568ea
P Nagamani <154883406+22hr1a0579@users.noreply.github.com>
2025-01-17 09:19:37 +0530

    Create ascii to integer.java

---
+++ equal/ascii to integer.java
@@ -0,0 +1,26 @@
+'''Write a program to convert given ascii value into integer
+
+Input Format
+
+an integer value
+
+Constraints
+
+no
+
+Output Format
+
+character'''
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt();
+        System.out.println((char)c);
+    
+    }
+}
