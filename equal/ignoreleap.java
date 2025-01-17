5195600973fada75980da921f6cfda248d4f8952
P Nagamani <154883406+22hr1a0579@users.noreply.github.com>
2025-01-17 09:12:00 +0530

    Create ignoreleap.java

---
+++ equal/ignoreleap.java
@@ -0,0 +1,27 @@
+'''Implement a program to convert given age into days, by considering 365 days as the length of the year and ignore leap years.
+
+Input Format
+
+age as input
+
+Constraints
+
+no
+
+Output Format
+
+number of days'''
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a*365);
+        
+    }
+}
