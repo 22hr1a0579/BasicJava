b8a373dbbb4ed0b876b1d5190196747e1b5af119
P Nagamani <154883406+22hr1a0579@users.noreply.github.com>
2025-01-17 08:35:56 +0530

    Create inc.java

---
+++ inc.java
@@ -0,0 +1,28 @@
+'''Implement a program that takes a number as a input, increment the number by +1 and print the result.
+
+Input Format
+
+an integer value
+
+Constraints
+
+no
+
+Output Format
+
+incremented value'''
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        p++;
+        System.out.println(p);
+    
+    }
+}
