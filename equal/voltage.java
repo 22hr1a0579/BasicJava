b02531a8c99f12c45cea34c40a105b5191804f27
P Nagamani <154883406+22hr1a0579@users.noreply.github.com>
2025-01-17 09:09:53 +0530

    Create voltage.java

---
+++ equal/voltage.java
@@ -0,0 +1,27 @@
+'''Implement a program to take voltage and current and print the calculated power.
+
+Input Format
+
+voltage and current
+
+Constraints
+
+no
+
+Output Format
+
+power'''
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a*b);
+      
+    }
+}
