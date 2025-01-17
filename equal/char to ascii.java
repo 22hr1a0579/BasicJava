fb0f702ffbeb2bb8a156373840c8f6fb81ef9aa1
P Nagamani <154883406+22hr1a0579@users.noreply.github.com>
2025-01-17 09:17:26 +0530

    Create char to ascii.java

---
+++ equal/char to ascii.java
@@ -0,0 +1,28 @@
+'''Write a program to convert given character into ascii value
+
+Input Format
+
+character
+
+Constraints
+
+no
+
+Output Format
+
+an integer value'''
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char c=sc.nextLine().charAt(0);
+        int ascii=c;
+        System.out.println(ascii);
+    
+      
+    }
+}
