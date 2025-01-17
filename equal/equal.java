6da3a447cbff81e6f35949e53e39c217ca10cd32
P Nagamani <154883406+22hr1a0579@users.noreply.github.com>
2025-01-17 09:08:00 +0530

    Create equal.java

---
+++ equal/equal.java
@@ -0,0 +1,26 @@
+'''Write a program to check whether the given two numbers are equal or not?
+
+Input Format
+
+two integer values
+
+Constraints
+
+no
+
+Output Format
+
+true or false'''
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a==b);
+    }
+}
